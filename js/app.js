d27fa4c13566ed15920384f0d8facc472a5dd1f8
sugey <valenciasue@hotmail.com>
2017-06-27 08:29:57 -0700

    adding constructor for images

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 
+'use strict';
+
+var categoryImg = [];
+
 
 function User(name, score) {
   this.imgName = imgName;
   this.path = score;
 };
+
+function ImageForCat(imgName, path, category) {
+  this.imgName = imgName;
+  this.path = path;
+  this.category = category;
+  categoryImg.push(this);
+};
+new ImageForCat('sports', 'img/sports.jpg', 'sports');
+new ImageForCat('geography', 'img/geography.jpg', 'geography');
+new ImageForCat('pop-culture', 'img/popculture.jpg', 'popculture');
