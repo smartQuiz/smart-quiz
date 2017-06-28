b3d0c363c21928dd1c2f785b9b14841264bd878e
Rami Bououni <rami.bououni@gmail.com>
2017-06-28 11:20:09 -0700

    quiz trigger for images, check answers, and keep scores

--- js/app.js
+++ js/app.js
@@ -5,7 +5,40 @@ var parentElement;
 var imageParent;
 var score = 0;
 var buttonParentElement;
+var clicked;
+var sportInputs = [];
+var travelInputs = [];
+var popInputs = [];
 
+var sportsQuestions = [
+  'Who won the last soccer world cup in Brazil 2014? Your choices are: a, b, c',
+  'How many times did USA made it to the world cup?',
+  'Who won the MLS cup in 2016?',
+  'Which famous player helped Napoli win their first ever championship?',
+  'Who won the champions league this year?'
+];
+var sportsAnswers = ['germany', '10', 'seattle sounders', 'diego maradona', 'real madrid'];
+var sportIds = ['germany', 'ten', 'seattlesounders', 'diegomaradona', 'realmadrid'];
+
+var travelQuestions = [
+  'What country is mt everest in? Your choices are: a, b, c',
+  'What is mt st helens famous for',
+  'What famous art museum is in Paris France?',
+  'Where are the most famous pyramids located?',
+  'What famous landmark that can be seen from space is in China?'
+];
+var travelAnswers = ['nepal', 'being a volcano', 'le louvre', 'egypt', 'the great wall'];
+var travelIds = ['nepal', 'beingavolcano', 'lelouvre', 'egypt', 'thegreatwall'];
+
+var popQuestions = [
+  'This singer sat in a Blank Space while trying to shake it off as she sang We are never getting back together. Who is she?',
+  'In the Big Bang Sheldon Cooper and Leonard Hofstadter lived next door to a pretty waitress named?',
+  'Queen Daenerys Targaryen is also known as the mother of ?',
+  'What famous actress plays Katniss Everdeen in the hunger games?',
+  'Doctor who thinks these are cool?'
+];
+var popAnswers = ['taylor swift', 'penny', 'dragons', 'jennifer lawrence', 'bow ties'];
+var popIds = ['taylorswift', 'penny', 'dragons', 'jenniferlawrence', 'bowties'];
 
 //create a user and grab it from the welcome page
 var user = document.getElementById('userName');
@@ -16,25 +49,15 @@ var user = document.getElementById('userName');
 
 function sportQuiz() {
 
-  var sportsQuestions = [
-    'Who won the last soccer world cup in Brazil 2014? Your choices are: a, b, c',
-    'How many times did USA made it to the world cup?',
-    'Who won the MLS cup in 2016?',
-    'Which famous player helped Napoli win their first ever championship?',
-    'Who won the champions league this year?'
-  ];
-  var sportsAnswers = ['Germany', '10', 'Seattle Sounders', 'Diego Maradona', 'Real Madrid'];
   var text;
   for (var i = 0; i < sportsQuestions.length; i++) {
 
     parentElement = document.getElementById('quiz');
 
-    sportsAnswers.forEach(function() {
+    sportIds.forEach(function() {
       text = document.createElement('input');
-      text.setAttribute('id', sportsAnswers[i]);
+      text.setAttribute('name', sportIds[i]);
     });
-
-    console.log(text.id);
 
     var label = document.createElement('label');
 
@@ -42,7 +65,7 @@ function sportQuiz() {
 
     parentElement.append(label);
     parentElement.append(text);
-
+    quiz.append(submitButton);
   }
 }
 
@@ -51,25 +74,15 @@ function sportQuiz() {
 
 function travelQuiz() {
 
-  var travelQuestions = [
-    'What country is mt everest in? Your choices are: a, b, c',
-    'What is mt st helens famous for',
-    'What famous art museum is in Paris France?',
-    'Where are the most famous pyramids located?',
-    'What famous landmark that can be seen from space is in China?'
-  ];
-  var travelAnswers = ['Nepal', 'Being a volcano', 'Le Louvre', 'Egypt', 'The Great Wall'];
   var text;
   for (var i = 0; i < travelQuestions.length; i++) {
 
     parentElement = document.getElementById('quiz');
 
-    travelAnswers.forEach(function() {
+    travelIds.forEach(function() {
       text = document.createElement('input');
-      text.setAttribute('id', travelAnswers[i]);
+      text.setAttribute('name', travelIds[i]);
     });
-
-    console.log(text.id);
 
     var label = document.createElement('label');
 
@@ -77,7 +90,7 @@ function travelQuiz() {
 
     parentElement.append(label);
     parentElement.append(text);
-
+    quiz.append(submitButton);
   }
 }
 
@@ -87,25 +100,15 @@ function travelQuiz() {
 
 function popQuiz() {
 
-  var popQuestions = [
-    'This singer sat in a Blank Space while trying to shake it off as she sang We are never getting back together. Who is she?',
-    'In the Big Bang Sheldon Cooper and Leonard Hofstadter lived next door to a pretty waitress named?',
-    'Queen Daenerys Targaryen is also known as the mother of ?',
-    'What famous actress plays Katniss Everdeen in the hunger games?',
-    'Doctor who thinks these are cool?'
-  ];
-  var popAnswers = ['Taylor Swift', 'Penny', 'Dragons', 'Jennifer Lawrence', 'Bow ties'];
   var text;
   for (var i = 0; i < popQuestions.length; i++) {
 
     parentElement = document.getElementById('quiz');
 
-    popAnswers.forEach(function() {
+    popIds.forEach(function() {
       text = document.createElement('input');
-      text.setAttribute('id', popAnswers[i]);
+      text.setAttribute('name', popIds[i]);
     });
-
-    console.log(text.id);
 
     var label = document.createElement('label');
 
@@ -113,14 +116,11 @@ function popQuiz() {
 
     parentElement.append(label);
     parentElement.append(text);
-
+    quiz.append(submitButton);
   }
 }
 
 // popQuiz();
-
-//=================================LINK EACH IMAGE TO APPROPRIATE QUIZ================================
-
 
 
 //========================================EVENT LISTENER===============================================
@@ -132,7 +132,7 @@ imageParent = document.getElementById('imageContainer');
 imageParent.addEventListener('click', clickHandler);
 
 function clickHandler() {
-  var clicked = event.target.getAttribute('id');
+  clicked = event.target.getAttribute('id');
   console.log('clicked ' + clicked);
   if (clicked === 'img1') {
     sportQuiz();
@@ -144,42 +144,119 @@ function clickHandler() {
     travelQuiz();
     imageParent.removeEventListener('click', clickHandler);
   }
-  //add the two button for the score and to reset:
-  // var resetButton = document.createElement('button');
-  // resetButton.innerHTML = 'Reset';
-  //
-  // //set the reset button to reload the page
-  // resetButton.addEventListener('click', function() {
-  //   location.reload();
-  // });
-  //
-  // //set the score button to take us to score page
-  // var scoreButton = document.createElement('button');
-  // scoreButton.innerHTML = 'Score';
-  // scoreButton.addEventListener('click', function() {
-  //   window.location.href = 'scoreboard.html';
-  // });
-  // parentElement.append(resetButton);
-  // parentElement.append(scoreButton);
-
-  var submitButton = document.createElement('button');
-  submitButton.innerHTML = 'Submit Answers';
-
-  //set the submit answers button to intake answers
-  submitButton.addEventListener('click', function(event) {
-    var answer = event.target.Germany.value;
-    var label = document.createElement('label');
-    label.textContent = answer;
-    if (answer === 'Germany') {
-      score++;
-    }
-    parentElement.append(label);
-    console.log(answer);
-    console.log(event.target);
-    // event.preventDefault();
-  });
-  parentElement.append(submitButton);
 }
+//========================================SUBMIT BUTTON===============================================
+
+//submit answers button
+var submitButton = document.createElement('button');
+submitButton.innerHTML = 'Submit Answers';
+
+var quizForm = document.getElementById('quiz');
+
+//set the submit answers button to intake answers
+quizForm.addEventListener('submit', function(event) {
+
+  event.preventDefault();
+
+  //======================LTRIGGER APPROPRIATE QUIZ FOR EACH IMAGE=========================
+  if (clicked === 'img1') {
+    //======================SPORT QUIZ=========================
+    var germany = event.target.germany.value.toLowerCase();
+    sportInputs.push(germany);
+    var ten = event.target.ten.value.toLowerCase();
+    sportInputs.push(ten);
+    var seattlesounders = event.target.seattlesounders.value.toLowerCase();
+    sportInputs.push(seattlesounders);
+    var diegomaradona = event.target.diegomaradona.value.toLowerCase();
+    sportInputs.push(diegomaradona);
+    var realmadrid = event.target.realmadrid.value.toLowerCase();
+    sportInputs.push(realmadrid);
+
+    for (var i = 0; i < sportInputs.length; i++) {
+      if (sportInputs[i] == sportsAnswers[i]) {
+        score++;
+      }
+    }
+    console.log(score);
+
+
+    console.log(germany);
+    console.log(ten);
+    console.log(seattlesounders);
+    console.log(diegomaradona);
+    console.log(realmadrid);
+
+  } else if (clicked === 'img2') {
+    //======================POP QUIZ=========================
+    var taylorswift = event.target.taylorswift.value.toLowerCase();
+    popInputs.push(taylorswift);
+    var penny = event.target.penny.value.toLowerCase();
+    popInputs.push(penny);
+    var dragons = event.target.dragons.value.toLowerCase();
+    popInputs.push(dragons);
+    var jenniferlawrence = event.target.jenniferlawrence.value.toLowerCase();
+    popInputs.push(jenniferlawrence);
+    var bowties = event.target.bowties.value.toLowerCase();
+    popInputs.push(bowties);
+
+    for (i = 0; i < popInputs.length; i++) {
+      if (popInputs[i] == popAnswers[i]) {
+        score++;
+      }
+    }
+    console.log(score);
+    console.log(taylorswift);
+    console.log(penny);
+    console.log(dragons);
+    console.log(jenniferlawrence);
+    console.log(bowties);
+
+  } else {
+    //======================TRAVEL QUIZ=========================
+    var nepal = event.target.nepal.value.toLowerCase();
+    travelInputs.push(nepal);
+    var beingavolcano = event.target.beingavolcano.value.toLowerCase();
+    travelInputs.push(beingavolcano);
+    var lelouvre = event.target.lelouvre.value.toLowerCase();
+    travelInputs.push(lelouvre);
+    var egypt = event.target.egypt.value.toLowerCase();
+    travelInputs.push(egypt);
+    var thegreatwall = event.target.thegreatwall.value.toLowerCase();
+    travelInputs.push(thegreatwall);
+
+    for (i = 0; i < travelInputs.length; i++) {
+      if (travelInputs[i] == travelAnswers[i]) {
+        score++;
+      }
+    }
+    console.log(score);
+    console.log(nepal);
+    console.log(beingavolcano);
+    console.log(lelouvre);
+    console.log(egypt);
+    console.log(thegreatwall);
+  }
+
+
+
+
+
+
+
+
+
+  // var answer = event.target.Germany.value;
+  // var label = document.createElement('label');
+  // label.textContent = answer;
+  // if (answer === 'Germany') {
+  //   score++;
+
+  // parentElement.append(label);
+
+
+});
+
+//========================================RESET AND RELOAD BUTTON===============================================
 
 buttonParentElement = document.getElementById('buttons');
 //add the two button for the score and to reset:
@@ -199,6 +276,26 @@ scoreButton.addEventListener('click', function() {
 });
 buttonParentElement.append(resetButton);
 buttonParentElement.append(scoreButton);
+
+//=============================GET INPUT DATAT AND SAVE SCORE=========================================
+
+
+// var submitButton = document.getElementById('quiz');
+//
+// submitButton.addEventListener('submit', function(){
+//
+//   //first quiz
+//
+//   var germany = event.target.Germany.value;
+//   console.log(germany);
+//
+// });
+
+
+
+
+
+
 
 
 // parentElement = document.getElementById('quiz');
@@ -236,11 +333,6 @@ buttonParentElement.append(scoreButton);
 //=====================================================================
 
 //
-
-
-
-
-
 
 
 function User(name, score, correct, wrong) {
